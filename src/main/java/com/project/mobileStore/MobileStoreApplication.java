8c372b10b2982b1810cc7db6f290911889aeb4c1
BettaibiMedAli <mohamedalibettaibi22@gmail.com>
2024-09-26 14:00:47 +0200

    initial commit

---
+++ src/main/java/com/project/mobileStore/MobileStoreApplication.java
@@ -0,0 +1,13 @@
+package com.project.mobileStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/mobileStore/MobileStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mobileStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
