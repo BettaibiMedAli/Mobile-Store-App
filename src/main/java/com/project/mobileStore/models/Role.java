cfab9e909e55c80a3f9c733fe73b334ed182cbf0
BettaibiMedAli <mohamedalibettaibi22@gmail.com>
2024-09-28 14:55:38 +0200

    added model classes

---
+++ src/main/java/com/project/mobileStore/models/Role.java
@@ -0,0 +1,36 @@
+package com.project.mobileStore.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    public Role()
+    {
+
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/project/mobileStore/models/User.java
@@ -0,0 +1,68 @@
+package com.project.mobileStore.models;
+
+import jakarta.persistence.*;
+
+import java.util.Collection;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String lastname;
+    private String email;
+    private Collection<Role> roles;
+    public User()
+    {
+
+    }
+
+    public User(String username, String lastname, String email, Collection<Role> roles) {
+        this.username = username;
+        this.lastname = lastname;
+        this.email = email;
+        this.roles = roles;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Collection<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Collection<Role> roles) {
+        this.roles = roles;
+    }
+}
